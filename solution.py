2761c5d0433502535f49090212cfcd520190f020
IBossman <121011537+IBossman@users.noreply.github.com>
2023-04-07 22:32:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,14 +4,19 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 563165218 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    # p - уровень доверия
+    # x - массив уровней значимости
+    # alpha - уровень значимости
+    
     alpha = 1 - p
     loc = x.mean()
+    quantile = alpha
     scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    return loc - scale * quantile, \
+           loc - scale * quantile
